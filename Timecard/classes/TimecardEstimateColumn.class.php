26aa565d12e33a846f894a015275363f2c3e9f80
Alexey Shumkin <Alex.Crezoff@gmail.com>
2012-05-22 15:40:04 +0400

    add sorting for "estimate" column

--- Timecard/classes/TimecardEstimateColumn.class.php
+++ Timecard/classes/TimecardEstimateColumn.class.php
@@ -20,8 +20,9 @@ class TimecardEstimateColumn extends MantisColumn {
 	public $title = '';
 
 	public $column = "estimate";
+	public $sortable = true;
 
-	public $sortable = false;
+	private $estimate_cache = array();
 
 	public function __construct() {
 		plugin_push_current( 'Timecard' );
@@ -31,14 +32,37 @@ class TimecardEstimateColumn extends MantisColumn {
 		plugin_pop_current();
 	}
 
+	public function cache( $p_bugs ) {
+		if ( count( $p_bugs ) < 1 ) {
+			return;
+		}
+
+		foreach ( $p_bugs as $t_bug ) {
+			$t_bug_estimate = TimecardBug::load( $t_bug->id );
+			$t_bug_estimate->calculate();
+			if ( $t_bug_estimate->estimate > 0 ) {
+			    $this->estimate_cache[ $t_bug->id ] = "$t_bug_estimate->spent/$t_bug_estimate->estimate";
+				if ( $t_bug_estimate->spent > $t_bug_estimate->estimate ) {
+					$this->estimate_cache[ $t_bug->id ] = '<span class="negative">' . $this->estimate_cache[ $t_bug->id ] . '</span>';
+				}
+			}
+		}
+	}
+
+	public function sortquery( $p_dir ) {
+		plugin_push_current( 'Timecard' );
+		$t_estimate_table = plugin_table( 'estimate' );
+		plugin_pop_current();
+		$t_bug_table = db_get_table( 'mantis_bug_table' );
+		return array('join' => "LEFT JOIN $t_estimate_table ON $t_estimate_table.bug_id = $t_bug_table.id",
+			'order' => "$t_estimate_table.estimate $p_dir", );
+	}
+
 	public function display( $p_bug, $p_columns_target ) {
 		plugin_push_current( 'Timecard' );
 
-		$p_bug_id = $p_bug->id;
-		$t_bug = TimecardBug::load( $p_bug_id, true );
-		$t_bug->calculate();
-		if ( $t_bug->estimate >= 0 ) {
-			echo $t_bug->estimate, plugin_lang_get( 'hours' );
+		if ( isset( $this->estimate_cache[ $p_bug->id ] ) ) {
+			echo $this->estimate_cache[ $p_bug->id ], plugin_lang_get( 'hours' );
 		}
 
 		plugin_pop_current();
